029469c8e18d84f883ba751277c3783cb320baab
Jakub Strakos <94374098+Efestrix@users.noreply.github.com>
2026-08-12 00:13:25 +0200

    Implement GuessNumber method using ternary search

---
+++ GuessNumberHigherOrLower.cs
@@ -0,0 +1,32 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution : GuessGame {
+    public int GuessNumber(int n) {
+        int left = 1, right = n;
+
+        while (true) {
+            int m1 = left + (right - left) / 3;
+            int m2 = right - (right - left) / 3;
+
+            if (guess(m1) == 0) return m1;
+            if (guess(m2) == 0) return m2;
+
+            if (guess(m1) + guess(m2) == 0) {
+                left = m1 + 1;
+                right = m2 - 1;
+            }
+            else if (guess(m1) == -1)
+                right = m1 - 1;
+            else
+                left = m2 + 1;
+        }
+        
+    }
+}
