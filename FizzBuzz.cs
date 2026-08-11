d2249bd6726c00e48ac104ab59569400c8c58a30
Jakub Strakos <94374098+Efestrix@users.noreply.github.com>
2026-08-11 23:58:22 +0200

    Update FizzBuzz.cs

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -6,21 +6,13 @@ public class Solution {
         for (int i = 1; i < n + 1; i++)
         {
             if (i % 3 == 0 && i % 5 == 0)
-            {
                 nums.Add("FizzBuzz");
-            }
             else if (i % 3 == 0)
-            {
                 nums.Add("Fizz");
-            }
             else if (i % 5 == 0)
-            {
                 nums.Add("Buzz");
-            }
             else
-            {
                 nums.Add(i.ToString());
-            }
         }
         return nums;
     }
